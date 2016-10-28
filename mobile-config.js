2337ea00e7df7657deb863988923d1a7c24de7d7
Niels Bantilan <niels@peggedsoftware.com>
2016-10-28 01:33:06 -0400

    added basic mobile config

---
+++ mobile-config.js
@@ -0,0 +1,9 @@
+App.info({
+  id: 'com.meteor.poopmo',
+  name: 'Poopmo',
+  version: "0.0.1"
+});
+
+App.setPreference('BackgroundColor', '0xff0000ff');
+App.setPreference('Orientation', 'default');
+App.setPreference('Orientation', 'all', 'ios');
